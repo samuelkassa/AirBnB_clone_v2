8ba4149ffa31deec08578d51cd961c59f7dc5890
= <=>
2023-09-10 11:45:01 -0700

    setting the server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.237.110.17', '54.172.213.241']
 
 
 def do_deploy(archive_path):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.237.110.17', '54.172.213.241']
 
 
 def do_pack():
